65aad14d6bf71f351b103dd53b2b82255fd46eaf
wbxing <wenbiaoxing@gmail.com>
2020-07-06 20:55:20 +0800

    delete something

--- demo_oss/src/test/java/com/education/oss/OSSTest.java
+++ demo_oss/src/test/java/com/education/oss/OSSTest.java
@@ -12,8 +12,8 @@ public class OSSTest {
         // Endpoint以杭州为例，其它Region请按实际情况填写。
         String endpoint = "http://oss-cn-shanghai.aliyuncs.com";
         // 阿里云主账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建RAM账号。
-        String accessKeyId = "LTAI4GD8sWmW94nBytPv24iQ";
-        String accessKeySecret = "rxYb823MVuHtVyBgmDdHpvTfgv4vfR";
+        String accessKeyId = "";
+        String accessKeySecret = "";
 
         // 创建OSSClient实例。
         OSSClient ossClient = new OSSClient(endpoint, accessKeyId, accessKeySecret);
